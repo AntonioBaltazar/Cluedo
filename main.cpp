dbefdf3a3996da77c9bd24103faa7de0ec970250
martindochamp <martinpaulcondet@gmail.com>
2021-10-27 21:41:15 +0200

    Choices menu

--- main.cpp
+++ main.cpp
@@ -6,11 +6,30 @@
 int main()
 {
     activateVirtualTerminal();
-    std::cout << "Hello world!" << std::endl;
-    std::cout << color("woww", Color::Bright_Blue) << " c'est super" << color(" c'est rouge", Color::Magenta)<<std::endl;
+    int saisie;
+    do
+    {
+        std::cout << color("Que voulez-vous faire ?", Color::Red) << std::endl;
+        std::cout << color("1.", Color::Bright_Red) << " Emma" << std::endl;
+        std::cout << color("2.", Color::Bright_Red) << " Tonio" << std::endl;
+        std::cout << color("3.", Color::Bright_Red) << " Martin (bg le gars)" << std::endl;
+        std::cin >> saisie;
+        switch (saisie)
+        {
+            case 1:
+                //Appel fct Emma
+                break;
+            case 2:
+                //Appel fct Tonio
+                break;
+            case 3:
+                //Appel fct Martin
+                break;
+            default: break;
+        }
+    } while (saisie > 0 && saisie < 4);
 
     std::vector<Card> MyPackage;
-
     MyPackage = card_shuffle();
 
     card_package_display(MyPackage);
