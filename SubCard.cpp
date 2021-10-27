80402fde74bbb2eb00cad3764611c1a40f1fdd38
Antonio <90786411+AntonioBaltazar@users.noreply.github.com>
2021-10-27 23:34:54 +0200

    Card package creation and shuffle 2.0

--- SubCard.cpp
+++ SubCard.cpp
@@ -4,44 +4,55 @@
 #include <time.h>
 #include "Utils.h"
 
-///Create a package of cards and shuffle them
-std::vector<Card> card_shuffle()
+///Creation of a card package
+std::vector<Card> card_creation()
 {
     std::vector<Card> MyPackage;
+    //Weapons cards
+    MyPackage.push_back( Card("weapon","pistolet") );
+    MyPackage.push_back( Card("weapon","corde")    );
+    MyPackage.push_back( Card("weapon","matraque") );
+
+    //Rooms cards
+    MyPackage.push_back( Card("planet","Saturne")   );
+    MyPackage.push_back( Card("planet","Uranus")   );
+    MyPackage.push_back( Card("planet","Venus")    );
+    MyPackage.push_back( Card("planet","Soleil")   );
+
+    //Suspects cards
+    MyPackage.push_back( Card("person","emma")     );
+    MyPackage.push_back( Card("person","Martin")   );
+    MyPackage.push_back( Card("person","Tonio")    );
+
+    return MyPackage;
+}
+
+///Shuffle a package of cards
+std::vector<Card> card_shuffle(std::vector<Card> MyPackage)
+{
     Card temp;
 
     srand(time(NULL));
 
-    ///Creation of cards
+    int alea = rand()%(30 - 15 + 1) + 15;
+    std::cout<<"Alea = "<<alea<<std::endl;
 
-    //Weapons cards
-     MyPackage.push_back( Card("weapon","pistolet") );
-     MyPackage.push_back( Card("weapon","corde")    );
-     MyPackage.push_back( Card("weapon","matraque") );
-
-    //Rooms cards
-     MyPackage.push_back( Card("planet","Sature")   );
-     MyPackage.push_back( Card("planet","Uranus")   );
-     MyPackage.push_back( Card("planet","Venus")    );
-     MyPackage.push_back( Card("planet","Soleil")   );
-
-    //Suspects cards
-     MyPackage.push_back( Card("person","emma")     );
-     MyPackage.push_back( Card("person","martin")   );
-     MyPackage.push_back( Card("person","tonio")    );
-
-     int alea = rand()%(30 - 15 + 1) + 15;
-     //Shuffle algorithm
-     for(int i = 0; i < alea; i++)
-     {
+    //Shuffle algorithm
+    for(int i = 0; i < alea; i++)
+    {
         int random1 = 0, random2 = 0;
-        do {    random1 = rand()%NBCARDS;   random2 = rand()%NBCARDS;   } while(random1 == random2);
+        do
+        {
+            random1 = rand()%NBCARDS;
+            random2 = rand()%NBCARDS;
+        }
+        while(random1 == random2);
 
         temp = MyPackage[random1];
         MyPackage[random1] = MyPackage[random2];
         MyPackage[random2] = temp;
 
-     }
+    }
 
     return MyPackage;
 }
@@ -51,10 +62,20 @@ void card_package_display(std::vector<Card> MyPackage)
 {
     std::vector<Card>::iterator it;
 
-    //
     for(it = MyPackage.begin(); it != MyPackage.end(); it++)
     {
         std::cout << color(it->getType(),Color::Blue);
         std::cout <<" "<< color(it->getName(),Color::Green)<<std::endl;
     }
+}
+
+
+///Optionnal
+void card_management()
+{
+    std::vector<Card> MyPackage;
+
+    MyPackage = card_creation(); //Mélange
+    MyPackage = card_shuffle(MyPackage); //Mélange
+    card_package_display(MyPackage); //Affichage
 }
--- main.cpp
+++ main.cpp
@@ -7,36 +7,34 @@
 int main()
 {
     activateVirtualTerminal();
-    int saisie;
+    int saisie = 0;
+
     do
     {
         std::cout << color("Que voulez-vous faire ?", Color::Red) << std::endl;
         std::cout << color("1.", Color::Bright_Red) << " Emma" << std::endl;
         std::cout << color("2.", Color::Bright_Red) << " Tonio" << std::endl;
-        std::cout << color("3.", Color::Bright_Red) << " Martin (bg le gars)" << std::endl;
+        std::cout << color("3.", Color::Bright_Red) << " Martin (bg le gars)  M'ouais" << std::endl;
+        std::cout << color("4.", Color::Bright_Red) << " Fini la seance de test" << std::endl;
         std::cin >> saisie;
         switch (saisie)
         {
             case 1:
-                //Appel fct Emma
+                ///Appel fct Emma
                 break;
             case 2:
-                //Appel fct Tonio
+                ///Appel fct Tonio
+                //Card Package Creation, Mixture and dislpay
+                card_management();
                 break;
             case 3:
-                //Appel fct Martin
+                ///Appel fct Martin
                 diceMenu();
                 break;
             default: break;
         }
-    } while (saisie > 0 && saisie < 4);
+    } while (saisie != 4);
 
-
-
-    ///Card Creation & Mixture
-    std::vector<Card> MyPackage;
-    MyPackage = card_shuffle(); //Mélange
-    card_package_display(MyPackage); //Affichage
 
     return 0;
 }
