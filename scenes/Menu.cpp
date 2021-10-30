fbd5a478f273c3063407419dff03300a7309490e
martindochamp <martinpaulcondet@gmail.com>
2021-10-30 21:02:57 +0200

    New types of animated files

--- scenes/Menu.cpp
+++ scenes/Menu.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 #include <conio.h>
 #include <windows.h>
 #include "Menu.h"
@@ -11,12 +12,13 @@ void Menu::render()
     system("cls");
     displayFile("menu");
     displayFile("menu2");
+    displayFile("menu3");
 }
 
 void Menu::displayFile(std::string fileName)
 {
     std::vector<std::string> cluedo;
-    bool timeable = true;
+    int timeable = 1;
 
     std::ifstream f;
     std::string content;
@@ -27,22 +29,49 @@ void Menu::displayFile(std::string fileName)
     while (std::getline(f, content))
     {
         if (content[0] == '@')
-            timeable = content[1];
-        else if (content[0] != '#')
-            cluedo.push_back(content);
+            timeable = (int)content[1];
+        else if (content[0] == '%')
+        {
+            content.erase(remove(content.begin(), content.end(), '%'), content.end());
 
+            HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleTextAttribute(H,std::stoi(strSplit(content, ":")[1])*16+std::stoi(strSplit(content, ":")[0]));
+
+        } else if (content[0] == '$')
+        {
+            content.erase(remove(content.begin(), content.end(), '$'), content.end());
+            std::vector<std::string> strs = strSplit(content, ":");
+            for (int i = std::stoi(strs[0]); i < std::stoi(strs[2]); i++)
+                for (int j = std::stoi(strs[1]); j < std::stoi(strs[3]); j++)
+                {
+                    gotoxy(i, j);
+                    std::cout << char(std::stoi(strs[4]));
+                    if (timeable == 1)
+                        Sleep(rand() % 50);
+                }
+        } else if (content[0] != '#') {
+            std::vector<std::string> strs = strSplit(content, ":");
+            gotoxy(std::stoi(strs[0]), std::stoi(strs[1]));
+            std::cout << char(std::stoi(strs[2]));
+            if (timeable)
+                Sleep(rand() % 50);
+        }
     }
+
+    HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(H,getBackgroundColor()*16+getTextColor());
 
     f.close();
 
-    for (size_t i = 0; i < cluedo.size(); i++) {
+    /*for (size_t i = 0; i < cluedo.size(); i++) {
         std::vector<std::string> strs = strSplit(cluedo[i], ":");
         gotoxy(std::stoi(strs[0]), std::stoi(strs[1]));
         std::cout << char(std::stoi(strs[2]));
         if (timeable)
             Sleep(rand() % 50);
-    }
+    }*/
 
     while(!kbhit());
+    int ch = getch();
 
 }
--- scenes/Menu.h
+++ scenes/Menu.h
@@ -2,9 +2,18 @@
 #define MENU_H_INCLUDED
 
 class Menu {
+private:
+    int m_textColor = 15;
+    int m_backgroundColor = 0;
 public:
     Menu() {}
     ~Menu() {}
+
+    int getTextColor() const { return m_textColor; }
+    int getBackgroundColor() const { return m_backgroundColor; }
+
+    void setTextColor(int textColor) { m_textColor = textColor; }
+    void setBackgroundColor(int backgroundColor) { m_backgroundColor = backgroundColor; }
 
     void render();
     void displayFile(std::string fileName);
